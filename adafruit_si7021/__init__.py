163a192121bf627b8d4d33cd1cf98ad8ff3f7972
foamyguy <foamyguy@gmail.com>
2023-04-17 15:28:38 -0500

    sleep after reset before read register

--- adafruit_si7021/__init__.py
+++ adafruit_si7021/__init__.py
@@ -25,6 +25,7 @@ Implementation Notes
 * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 """
 import struct
+import time
 
 from adafruit_bus_device.i2c_device import I2CDevice
 from micropython import const
@@ -137,6 +138,7 @@ class SI7021:
     def __init__(self, i2c_bus: I2C, address: int = 0x40) -> None:
         self.i2c_device = I2CDevice(i2c_bus, address)
         self._command(_RESET)
+        time.sleep(0.015)
         # Make sure the USER1 settings are correct.
         while True:
             # While restarting, the sensor doesn't respond to reads or writes.
